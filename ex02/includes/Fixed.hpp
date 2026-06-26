9f2310e26a8be50757abd0ff3928aace48bdfbb1
SophiePelet <peletsophie@gmail.com>
2026-06-26 19:23:45 +0200

    feat: add overloading operators functions and comparison functions to the Fixed class

---
+++ ex02/includes/Fixed.hpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 14:13:54 by sopelet           #+#    #+#             */
+/*   Updated: 2026/06/26 19:22:30 by sophie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cmath>
+#include <iostream>
+
+class	Fixed	{
+	public:
+
+	Fixed();
+	Fixed(const int nb);
+	Fixed(const float nb);
+	~Fixed();
+	Fixed(const Fixed& other);
+	Fixed &operator=(const Fixed& other);
+	
+	int getRawBits() const;
+	void setRawBits(int const raw);
+
+	float	toFloat(void) const;
+	int		toInt(void) const;
+
+	// Overloading comparison operators
+	bool	operator>(const Fixed& instance);
+	bool	operator<(const Fixed& instance);
+	bool	operator>=(const Fixed& instance);
+	bool	operator<=(const Fixed& instance);
+	bool	operator==(const Fixed& instance);
+	bool	operator!=(const Fixed& instance);
+
+	// Overloading arithmetic operators
+	bool	operator+(const Fixed& instance);
+	bool	operator-(const Fixed& instance);
+	bool	operator*(const Fixed& instance);
+	bool	operator/(const Fixed& instance);
+
+	// Overloading pre-increment/pre-decrement
+	Fixed&	operator++();
+	Fixed&	operator--();
+	
+	// Overlaoding post-increment/post-decrement
+	Fixed	operator++(int);
+	Fixed	operator--(int);
+	
+	// Comparison functions
+	static Fixed&	min(Fixed& n1, Fixed& n2);
+	static Fixed&	min(const Fixed n1, const Fixed n2);
+	static Fixed&	max(Fixed& n1, Fixed& n2);
+	static Fixed&	max(const Fixed n1, const Fixed n2);;
+
+	private:
+
+	int _fixedPoint;
+	static const int _bits=8;
+};
+
+/******************************* Global functions ***************************/
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& instance);
---
+++ ex02/src/Fixed.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sophie <sophie@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/24 14:01:48 by sopelet           #+#    #+#             */
+/*   Updated: 2026/06/26 19:16:43 by sophie           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+// Constructors/destructors, copy constructor and copy assignment operator
+Fixed::Fixed() : _fixedPoint(0) { std::cout << "Default constructor called\n"; }
+/*
+	shifts all the int bits to the left by 8 postions, to convert it to a
+	fixed-point configuration
+	the last 8 bits will then be decimal
+*/
+Fixed::Fixed(const int nb) : _fixedPoint(nb << _bits) { std::cout << "Int constructor called\n"; }
+/*
+	cannot use bitwise shift operators on floating-point numbers in C++
+	mutiply by 256 instead
+	`roundf` is necessary because int truncation drops the decimal part instead of
+	rounding it.
+	`* 256` is a "packing" operation, it shifts the fractional part into the int area so they
+	aren't lost when save into a standard `int`
+*/
+Fixed::Fixed(const float nb) : _fixedPoint(roundf(nb * 256)) { std::cout << "Float constructor called\n"; }
+Fixed::~Fixed() { std::cout << "Destructor called\n"; }
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called\n";
+	this->_fixedPoint = other.getRawBits();
+}
+Fixed &Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called\n";
+	if (this != &other)
+		this->_fixedPoint = other.getRawBits();
+	return (*this);
+}
+
+// Getter and setter functions
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called\n";
+	return (this->_fixedPoint);
+}
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called\n";
+	this->_fixedPoint = raw;
+}
+
+// Conversion functions
+float	Fixed::toFloat(void) const { return ((float)_fixedPoint / 256); }
+int		Fixed::toInt(void) const { return (_fixedPoint >> _bits); }
+
+/*
+	Overload of the insertion operator (<<):
+	Inserts a floating-point representation of the fixed-point number 
+	into the output stream object passed as a parameter
+	Couldn't be printed without the overload, as the insertion operator can't print objects
+*/
+std::ostream&	operator<<(std::ostream& stream, const Fixed& instance) {
+	stream << instance.toFloat();
+	return (stream);
+}
+

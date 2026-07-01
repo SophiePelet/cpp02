737777b830f4323fb22f457458b688af2809f547
Sophie <peletsophie@gmail.com>
2026-07-01 14:06:32 +0200

    fix: correct `Point` typo

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 17:45:19 by sopelet           #+#    #+#             */
-/*   Updated: 2026/07/01 12:00:26 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/07/01 14:06:17 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ int	main(void) {
 	Point	a(0.0f, 0.0f);
 	Point	b(6.0f, 0.0f);
 	Point	c(0.0f, 6.0f);
-	Point	Point(1.0f, 1.0f);
+	Point	point(1.0f, 1.0f);
 	//Point	Point(6.0f, 6.0f);
 	//Point	Point(12.0f, 12.0f);
 
-	bool	is_in = bsp(a, b, c, Point);
+	bool	is_in = bsp(a, b, c, point);
 	std::cout << is_in << std::endl;
 	
 	return (0);
--- ex03/src/bsp.cpp
+++ ex03/src/bsp.cpp
@@ -6,16 +6,16 @@
 /*   By: sopelet <sopelet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/27 17:08:12 by sopelet           #+#    #+#             */
-/*   Updated: 2026/06/27 18:05:07 by sopelet          ###   ########.fr       */
+/*   Updated: 2026/07/01 14:05:17 by sopelet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-bool	bsp(Point const a, Point const b, const Point c, Point const Point) {
-    Fixed cross1 = (b.getX() - a.getX()) * (Point.getY() - a.getY()) - (b.getY() - a.getY()) * (Point.getX() - a.getX());
-    Fixed cross2 = (c.getX() - b.getX()) * (Point.getY() - b.getY()) - (c.getY() - b.getY()) * (Point.getX() - b.getX());
-    Fixed cross3 = (a.getX() - c.getX()) * (Point.getY() - c.getY()) - (a.getY() - c.getY()) * (Point.getX() - c.getX());
+bool	bsp(Point const a, Point const b, Point const c, Point const point) {
+    Fixed cross1 = (b.getX() - a.getX()) * (point.getY() - a.getY()) - (b.getY() - a.getY()) * (point.getX() - a.getX());
+    Fixed cross2 = (c.getX() - b.getX()) * (point.getY() - b.getY()) - (c.getY() - b.getY()) * (point.getX() - b.getX());
+    Fixed cross3 = (a.getX() - c.getX()) * (point.getY() - c.getY()) - (a.getY() - c.getY()) * (point.getX() - c.getX());
 
     bool has_neg = (cross1 < Fixed(0)) || (cross2 < Fixed(0)) || (cross3 < Fixed(0));
     bool has_pos = (cross1 > Fixed(0)) || (cross2 > Fixed(0)) || (cross3 > Fixed(0));
